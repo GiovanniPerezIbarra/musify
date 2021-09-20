a52af181d31b6de79d2243786298c33932c5b7e3
Giovanni Pérez Ibarra <giorapni@gmail.com>
2021-09-20 15:33:24 -0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,7 +2,6 @@
 var mongoose = require('mongoose');
 var app = require('./app');
 var port = process.env.PORT || 3977;
-var atlas = "mongodb://admin:admin@cluster0-shard-00-00-euqmy.mongodb.net:27017,cluster0-shard-00-01-euqmy.mongodb.net:27017,cluster0-shard-00-02-euqmy.mongodb.net:27017/curso_mean2?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true";
 var local = 'mongodb://localhost:27017/curso_mean2';
  //mongoose.Promise = global.Promise;
 mongoose.connect(local, (err,res) => {
