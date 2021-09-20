b98df23ac37ae807cff38280278f9afd70a0a5ae
Giovanni Pérez Ibarra <giorapni@gmail.com>
2021-09-20 15:18:20 -0500

    musify
    MEAN Stack

---
+++ api/index.js
@@ -0,0 +1,19 @@
+'use strict'
+var mongoose = require('mongoose');
+var app = require('./app');
+var port = process.env.PORT || 3977;
+var atlas = "mongodb://admin:admin@cluster0-shard-00-00-euqmy.mongodb.net:27017,cluster0-shard-00-01-euqmy.mongodb.net:27017,cluster0-shard-00-02-euqmy.mongodb.net:27017/curso_mean2?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true";
+var local = 'mongodb://localhost:27017/curso_mean2';
+ //mongoose.Promise = global.Promise;
+mongoose.connect(local, (err,res) => {
+    if(err){
+        //throw err;
+        console.log('Problema con la conexión hacia MongoDB');
+    }else{
+        console.log('La conexión a la base de datos se hizo correctamente');
+
+        app.listen(port,function(){
+            console.log("Servidor del API Rest escuchando en el puerto: " + port);
+        });
+    }
+});
